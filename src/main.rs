2e936f540423db962cb139997562bbc059d324bd
Pablo Lozano <4032486+Altaflux@users.noreply.github.com>
2024-10-23 15:08:51 -0700

    Performance test

--- src/main.rs
+++ src/main.rs
@@ -6,8 +6,6 @@ use embedded_sdmmc::{SdCard, VolumeManager};
 use esp_backtrace as _;
 use esp_hal::{
     delay::Delay,
-    dma::{Dma, DmaPriority},
-    dma_tx_buffer,
     gpio::{Io, Level, Output},
     peripherals::TIMG0,
     prelude::*,
@@ -17,14 +15,8 @@ use esp_hal::{
 };
 use gameboy::{display::GameboyLineBufferDisplay, GameEmulationHandler, NullButtonHandler};
 use gb_core::gameboy::GameBoy;
-use hardware::{
-    display::{
-        dma_streamer::{DmaStreamer, SpiDmaCMInterface},
-        scaler::ScreenScaler,
-    },
-    sound::NullAudioPlayer,
-};
-use ili9341::{DisplaySize, DisplaySize240x320};
+use hardware::sound::NullAudioPlayer;
+
 mod gameboy;
 mod hardware;
 mod util;
@@ -48,7 +40,7 @@ fn init_heap() {
 fn main() -> ! {
     #[allow(unused)]
     let peripherals = esp_hal::init(esp_hal::Config::default());
-    let mut delay = Delay::new();
+    let delay = Delay::new();
 
     init_heap();
 
@@ -62,11 +54,6 @@ fn main() -> ! {
     let mut led = Output::new(io.pins.gpio47, Level::Low);
 
     led.set_high();
-
-    const SCREEN_WIDTH: usize =
-        (<DisplaySize240x320 as DisplaySize>::WIDTH as f32 / 1.0f32) as usize;
-    const SCREEN_HEIGHT: usize =
-        (<DisplaySize240x320 as DisplaySize>::HEIGHT as f32 / 1.0f32) as usize;
 
     log::info!("START ROM LOAD");
     /////////SDCARD
@@ -109,68 +96,17 @@ fn main() -> ! {
     core::mem::drop(boot_rom_data);
     log::info!("ROM DATA LOADED");
 
-    //////////SCREEN SETUP
-    // ANCHOR: init-dma
-    // we need to create the DMA driver and get a channel
-    let dma = Dma::new(peripherals.DMA);
-    let dma_channel = dma.channel0;
-
-    let main_screen_buffer = dma_tx_buffer!(32000).unwrap();
-    let spare_screen_buffer = dma_tx_buffer!(32000).unwrap();
-
-    let mosi = io.pins.gpio4;
-    let sclk = io.pins.gpio5;
-    //
-    let reset = Output::new(io.pins.gpio6, Level::Low);
-    let dc = Output::new(io.pins.gpio7, Level::Low);
-
-    // ANCHOR: configure-spi
-    // we can call `.with_dma` on the SPI driver to make it use DMA
-    let spi = Spi::new(peripherals.SPI2, 300.MHz(), SpiMode::Mode0)
-        .with_sck(sclk)
-        .with_mosi(mosi)
-        .with_dma(dma_channel.configure(true, DmaPriority::Priority9));
-
-    let streamer = DmaStreamer::new(spi, main_screen_buffer, spare_screen_buffer);
-    let display_interface = SpiDmaCMInterface::new(streamer, dc);
-    let mut display = ili9341::Ili9341::new(
-        display_interface,
-        reset,
-        &mut delay,
-        ili9341::Orientation::LandscapeFlipped,
-        ili9341::DisplaySize240x320,
-    )
-    .unwrap();
-
     //GAMEBOY INIT
     let screen = GameboyLineBufferDisplay::new(Box::new(timer0));
     let mut gameboy = GameBoy::create(screen, cartridge, boot_rom, Box::new(NullAudioPlayer));
-    let scaler: ScreenScaler<144, 160, { SCREEN_WIDTH }, { SCREEN_HEIGHT }> = ScreenScaler::new();
-    ////
-    display.clear_screen(0xf9b0).unwrap();
 
     let mut loop_counter: usize = 0;
     let mut sample_count: usize = 0;
     loop {
-        // display.clear_screen(0xf9b0).unwrap();
-        // display.clear_screen(0x423f).unwrap();
-        // log::info!("Hello world!");
         let start_time = esp_hal::time::now();
-        for x in (GameEmulationHandler::new(&mut gameboy, &mut NullButtonHandler)) {
+        for _x in GameEmulationHandler::new(&mut gameboy, &mut NullButtonHandler) {
             sample_count = sample_count.saturating_add(1);
         }
-        // display
-        //     .draw_raw_iter(
-        //         0,
-        //         0,
-        //         (SCREEN_HEIGHT - 1) as u16,
-        //         (SCREEN_WIDTH - 1) as u16,
-        //         scaler.scale_iterator(GameEmulationHandler::new(
-        //             &mut gameboy,
-        //             &mut NullButtonHandler,
-        //         )),
-        //     )
-        //     .unwrap();
 
         let end_time = esp_hal::time::now();
         let diff = end_time - start_time;
